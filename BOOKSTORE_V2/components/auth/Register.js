c8965a378346331db3c4f152e83ae5920d55c1b8
trnghia <nguyentrongnghia7949@gmail.com>
2023-12-19 11:04:01 +0700

    create account

--- BOOKSTORE_V2/components/auth/Register.js
+++ BOOKSTORE_V2/components/auth/Register.js
@@ -1,13 +1,57 @@
-import { Switch ,View, Text, ScrollView, TouchableOpacity, Image, TextInput ,SafeAreaView  } from 'react-native';
+import { Switch ,View, Text, ScrollView, TouchableOpacity, Image, TextInput ,SafeAreaView ,Alert } from 'react-native';
 import React, {useState} from 'react';
 import {FontAwesome, Ionicons} from '@expo/vector-icons'
 import styles from './register.styles';
 import { themeColors, themeSize } from '../../constants/theme';
-
-
+import { useNavigation } from '@react-navigation/native';
+import API_CONFIG from '../../config'
+import axios from 'axios';
+import { faUser } from '@fortawesome/free-solid-svg-icons'
 export default function Register() {
+
+  const navigation = useNavigation();
   const [isEnabled, setIsEnabled] = useState(false);
   const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [confirmpassword, setConfirmPassword] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [firstname, setFirstname] = React.useState('');
+  const [lastname, setLastname] = React.useState('');
+
+  const user = {
+    first_name: firstname,
+    last_name: lastname,
+    username: username,
+    email: email,
+    password1: password,
+    password2: confirmpassword
+  };
+  const handleRegister = async () => {
+   
+    try {
+        const response = await axios({
+            method: 'post',
+            url: `${API_CONFIG.HOST}${API_CONFIG.REGISTER}`,
+            headers: { 'Content-Type': 'application/json' },
+            data: user
+        });
+        if (response.data.status === 'success') {
+          console.log(response.data.message);
+          Alert.alert(response.data.message);
+          navigation.navigate('Login');
+        } else if (response.data.status === 'error') {     
+            Alert.alert(response.data.error);
+            console.error(response.data.message); 
+            console.error(response.data.error);
+        }
+    } catch (error) {
+      console.log(error);
+      console.error("Error from API: ", error.response.data);
+    }
+};
+
   return (
     <SafeAreaView>
       <ScrollView>
@@ -18,32 +62,80 @@ export default function Register() {
           <Text style={styles.text}>Create an account to experience now </Text> 
           
         </View>
+
         <View style={styles.midView}>
+
           <View style={styles.inputStyle}>
             <FontAwesome style={styles.inputStyleIcon} name='user-o' size={20}></FontAwesome>
-            <TextInput style={styles.inputStyleText} placeholder='Enter your name'></TextInput>
+            <TextInput 
+            style={styles.inputStyleText}
+            placeholder='Enter user name'
+            onChangeText={text => setUsername(text)}
+            value={username}
+            ></TextInput>
           </View>   
+
           <View style={styles.inputStyle}>
+            <FontAwesome style={styles.inputStyleIcon} name='user-o' size={20}></FontAwesome>
+            <TextInput
+             style={styles.inputStyleText}
+             placeholder='Enter your first name'
+             onChangeText={text => setFirstname(text)}
+             value={firstname}
+             ></TextInput>
+          </View>   
+
+          <View style={styles.inputStyle}>
+            <FontAwesome style={styles.inputStyleIcon} name='user-o' size={20}></FontAwesome>
+            <TextInput style={styles.inputStyleText}
+             placeholder='Enter your last name'
+             onChangeText={text => setLastname(text)}
+             value={lastname}
+             ></TextInput>
+          </View>   
+
+          {/* <View style={styles.inputStyle}>
             <Ionicons style={styles.inputStyleIcon} name='phone-portrait-outline' size={20}></Ionicons>
             <TextInput style={styles.inputStyleText} placeholder='Enter your phone number'></TextInput>
           </View> 
+
           <View style={styles.inputStyle}>
             <Ionicons style={styles.inputStyleIcon} name='location-outline' size={20}></Ionicons>
             <TextInput style={styles.inputStyleText} placeholder='Enter your address'></TextInput>
-          </View> 
+          </View>  */}
+
           <View style={styles.inputStyle}>
             <Ionicons style={styles.inputStyleIcon} name='mail-outline' size={20}></Ionicons>
-            <TextInput style={styles.inputStyleText} placeholder='Enter your Email'></TextInput>
+            <TextInput
+             style={styles.inputStyleText}
+             placeholder='Enter your Email'
+             onChangeText={text => setEmail(text)}
+             value={email}
+             ></TextInput>
           </View>   
+          
           <View style={styles.inputStyle}>
             <Ionicons style={styles.inputStyleIcon} name='lock-closed-outline' size={20}></Ionicons>
-            <TextInput secureTextEntry style={styles.inputStyleText} placeholder='Enter password'></TextInput>
+            <TextInput
+             secureTextEntry
+              style={styles.inputStyleText}
+              placeholder='Enter password'
+              onChangeText={text => setPassword(text)}
+              value={password}
+              ></TextInput>
           </View>  
+
           <View style={styles.inputStyle}>
             <Ionicons style={styles.inputStyleIcon} name='lock-closed-outline' size={20}></Ionicons>
-            <TextInput secureTextEntry style={styles.inputStyleText} placeholder='Confirm password'></TextInput>
-          </View>   
-              
+            <TextInput
+             secureTextEntry
+              style={styles.inputStyleText}
+              placeholder='Confirm password'
+              onChangeText={text => setConfirmPassword(text)}
+              value={confirmpassword}
+              ></TextInput>
+          </View> 
+
         </View>      
         <View style={styles.bottomView}>      
           <View style={styles.terms}>
@@ -55,11 +147,12 @@ export default function Register() {
             </View>          
           </View>
           
-          <TouchableOpacity onPress={()=>{}}>
+          <TouchableOpacity onPress={handleRegister}>
             <View style={styles.signInStyle}>
               <Text style={styles.signInStyleText}>Create</Text>
             </View>  
           </TouchableOpacity>
+
         </View>
       </View>      
     </ScrollView>
--- BOOKSTORE_V2/config.js
+++ BOOKSTORE_V2/config.js
@@ -1,6 +1,8 @@
 
  export default  API_CONFIG = {
-    HOST: 'http://192.168.1.11:8000', // Thay thế bằng địa chỉ host của API 
+    HOST: 'http://192.168.1.8:8000', // Thay thế bằng địa chỉ host của API 
+    
     LOGIN: '/user/login/', 
+    REGISTER:'/user/register/'
   };
   
